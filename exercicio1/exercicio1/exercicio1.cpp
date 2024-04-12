b3e637b7d8681f89f606ea0b427403f3b740b837
cruzz05 <133364197+cruzz05@users.noreply.github.com>
2024-04-12 15:43:55 -0300

    Add files via upload

---
+++ exercicio1/exercicio1/exercicio1.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+int main() {
+	
+	/*int instalacao = 300;
+	int limpeza = 150;
+	float manutencao = 499.99;
+	float soma = instalacao + limpeza + manutencao;
+	
+	cout << soma;
+	
+	return 0;*/
+
+	/*int salario = 35;
+	int horas = 8;
+	
+	int mult = salario * horas;
+
+	cout << mult;
+
+	return 0; */
+
+	/*int doces = 27;
+	int caixas = 16;
+
+	int mult = doces * caixas;
+
+	cout << mult;
+
+	return 0;*/
+
+	/*int garrafas = 300;
+	int caixas = 20;
+
+	int div = garrafas / caixas;
+
+	cout << div;
+
+	return 0;*/
+
+		
+		/*int camisetas = 120;
+		int vendidas = 80;
+		int compradas = 50;
+		float preco = 30.00;
+
+		int total_camisetas = camisetas - vendidas + compradas;
+
+		float faturamento = vendidas * preco;
+
+		cout << "Total de camisetas agora: " << total_camisetas;
+		cout << "\n";
+		cout << "Faturamento total: R$ " << faturamento;
+
+		return 0;*/
+	}
+
+
